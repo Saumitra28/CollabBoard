d4d6bd8ef8ba27a6293ec84438ec3d785195eff0
Madhav <madhavbsnl013@gmail.com>
2024-07-25 20:38:28 +0530

    chart adjusted

--- src/Components/GooglesheetsChart/GooglesheetsChart.js
+++ src/Components/GooglesheetsChart/GooglesheetsChart.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import "./GoogleSheetChart.css"; // Import a separate CSS file for this component
 
 const GoogleSheetChart = () => {
   return (
-    <div className='w-full' style={{ display: 'flex', justifyContent: 'center', margin: '20px 0' }}>
-<iframe width="345" height="266" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRykhdaBCpXu7SyuErtEfhZk3l39-LzCDLx9oahPwmtqWxcSuiEZV3nLjH4uZE9yXLaoqUYx5D2T2QD/pubchart?oid=1953218487&amp;format=interactive"></iframe>    </div>
+    <div className="google-sheet-chart-container">
+      <iframe
+        className="google-sheet-chart"
+        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRykhdaBCpXu7SyuErtEfhZk3l39-LzCDLx9oahPwmtqWxcSuiEZV3nLjH4uZE9yXLaoqUYx5D2T2QD/pubchart?oid=1953218487&amp;format=interactive"
+      ></iframe>
+    </div>
   );
 };
 
