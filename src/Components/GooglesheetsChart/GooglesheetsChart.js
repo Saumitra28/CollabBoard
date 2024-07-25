ad14f1594a605131ac3d0add0377f34c5f330821
Saumitra <saumitraupadhyay28@gmail.com>
2024-07-25 23:13:09 +0530

    Update GoogleSheetChart CSS and component height

--- src/Components/GooglesheetsChart/GooglesheetsChart.js
+++ src/Components/GooglesheetsChart/GooglesheetsChart.js
@@ -3,12 +3,14 @@ import "./GoogleSheetChart.css"; // Import a separate CSS file for this componen
 
 const GoogleSheetChart = () => {
   return (
-    <div className="google-sheet-chart-container">
+   <div className="sheet-wrapper">
+     <div className="google-sheet-chart-container">
       <iframe
         className="google-sheet-chart"
         src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRykhdaBCpXu7SyuErtEfhZk3l39-LzCDLx9oahPwmtqWxcSuiEZV3nLjH4uZE9yXLaoqUYx5D2T2QD/pubchart?oid=1953218487&amp;format=interactive"
       ></iframe>
     </div>
+   </div>
   );
 };
 
